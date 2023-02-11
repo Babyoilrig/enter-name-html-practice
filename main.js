d3e227cb3f6b9e66764d143e94698e9330ebc797
babyoilrig <catrin.mentzoni@yahoo.com>
2023-02-11 20:22:52 +0000

    Added input and button that calls function on click and console.logs input value

---
+++ main.js
@@ -0,0 +1,44 @@
+// let correctPassword = "admin123";
+// let attempts = 0;
+
+// function checkPassword(){
+    
+//     while (attempts <3){
+//         let userPassword = prompt("What's your password?");
+//        if(userPassword === correctPassword){
+//         alert("Correct!");
+//         return true;
+//        } 
+//        else{
+//         alert("Incorrect!");
+//        }
+//        attempts++;
+//     }
+//     return false;
+// };
+// checkPassword();
+
+// const button = document.getElementById("submitButton").addEventListener("click", handleClick);
+// // let nameText = "";
+
+// function handleClick() {
+//     console.log("Click event fired");
+// }
+
+    // const textInputElement = document.getElementById("nameInput").addEventListener("keyup", handleChange);
+
+    // function handleChange() {
+    //     let name = textInputElement.value;
+    //     console.log(name);
+    // };
+    function getInputValue(){
+        var inputVal = document.getElementById("nameInput").value;
+        console.log(inputVal);
+    }
+
+    
+
+
+    
+
+
